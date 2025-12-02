8e343b855e293677a1fcbef633eb80944aaf685e
Tolga <116166070+Tolga8702@users.noreply.github.com>
2025-12-02 10:47:58 +0300

    Add number guessing game to main.py
    Implemented a number guessing game with three attempts.

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import random
 
-print (random.randint(1,100))
+print("=== Sayı Tahmin Oyunu ===")
+print("1 ile 100 arasında bir sayı tuttum. 3 tahmin hakkın var!")
 
-print ('tuttumu ?')
+sayi = random.randint(1, 100)
+hak = 3
+
+while hak > 0:
+    tahmin = int(input("Tahminin: "))
+
+    if tahmin == sayi:
+        print(":) Tebrikler! Doğru bildin!")
+        break
+    elif tahmin < sayi:
+        print("Yukarı çık!")
+    else:
+        print("Aşağı in!")
+
+    hak -= 1
+    print("Kalan hakkın:", hak)
+
+if hak == 0:
+    print(":( Tahmin hakkın bitti! Tutulan sayı:", sayi)
